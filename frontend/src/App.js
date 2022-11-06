9f92a8ac74f791452b4cd98b2bd02f048354c11f
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2022-11-06 14:58:48 +0200

    Workshop FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react';
-import axios from 'axios';
+import {useState} from 'react';
 
 function App() {
-    const [temp, setTemp] = useState("");
-    useEffect(() => {
-        async function f() {
-            setTemp(await axios.get("http://localhost:5000/"));
+    let initIndex = 0;
+    const [todo, setTodo] = useState([
+        {
+            id: ++initIndex,
+            text:"Dragos este sexy",
+            done: true
+        },{
+            id: ++initIndex,
+            text:"Dragos este extrem de sexy",
+            done: false
+        },{
+            id: ++initIndex,
+            text:"Codrut este zeul meme-urilo",
+            done: false
+        },{
+            id: ++initIndex,
+            text:"Pirvulet was here!",
+            done: true
+        }, {
+            id: ++initIndex,
+            text: "Acesta este doar un text foarte lung pentru a testa! Mi-a fost lene sa caut pe Google Lorem Ipsum-ul",
+            done: true
+        },
+    ])
+    const [index, setIndex] = useState(initIndex + 1)
+    function changeButtonState(id) {
+        const localTodo = todo
+        localTodo.map(item=>{
+            if(item.id === id){
+                item.done = !item.done;
+            }
+            return item;
+        })
+        setTodo([...localTodo])
+    }
 
-        }
-        f();
-    }, []);
+    function removeToDo(item) {
+        const index = todo.indexOf(item);
+        let localTodo = todo
+        localTodo.splice(index,1)
+        setTodo([...localTodo])
+    }
 
-  return (
+    function addNewToDo(e) {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const text = formData.get("todoAdder")
+        setTodo([...todo,{
+            id: index,
+            text,
+            done: false
+        }])
+        setIndex(index + 1)
+        document.getElementById("todoAdder").setAttribute("text","")
+    }
+
+    return (
     <div className="App">
-        <p>
-            {JSON.stringify(temp)}
-        </p>
+        <h1>To Do App - Workshop IT</h1>
+        <form action="" onSubmit={addNewToDo}>
+            <fieldset>
+                <legend>Adauga un nou Item</legend>
+                <label htmlFor="todoAdder">Add new item</label><br/>
+                <input type="text" name="todoAdder" id="todoAdder" placeholder={"Tasteaza aici."} required={true}/><br/>
+                <button type="submit">Adauga</button>
+            </fieldset>
+        </form>
+        <ul>
+            {todo.map(item=>{
+                return (
+                <li className={item.done ? "finished" : "notFinished"} key={item.id}>
+                    {`${item.id}: ${item.text}`}
+                    <span>
+                        <button onPointerDown={() => changeButtonState(item.id)}>Mark as {item.done? "unfinished": "finished"}</button>
+                        <button onPointerDown={() => removeToDo(item)}>Delete</button>
+                    </span>
+                </li>
+                )
+            })}
+        </ul>
     </div>
   );
 }
